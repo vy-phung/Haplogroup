b3ff48f31c587dafd93c6479a7f1d25e54519c33
Vy K Phung <vyphung1901@gmail.com>
2024-03-27 07:31:25 +0700

    Delete finalCodes/bash scripts codes directory

--- finalCodes/bash scripts codes/bash1_22_6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DataList=extra.txt
-Field_Separator=$IFS
-IFS=,
-for val in `cat $DataList`; do cp /content/drive/MyDrive/RetrieveData/Dataset1/Malaysia/fasta1/$val /content/drive/MyDrive/RetrieveData/Dataset1/Malaysia/fasta/$val
-cp  /content/drive/MyDrive/RetrieveData/Dataset1/Malaysia/fasta1/$val /content/drive/MyDrive/RetrieveData/Dataset2/Malaysia/fasta/$val 
-cat /content/drive/MyDrive/RetrieveData/Dataset1/Malaysia/Malay/Malaysia_seq_name.txt  | grep "complete genome" | awk '{print$1}' > complete.txt 
-done
